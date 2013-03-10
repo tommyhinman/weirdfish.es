28353b82e4f7f78faedadf534efb3e2aee1fae1c
tommyhinman@gmail.com <tommyhinman@gmail.com>
2013-03-09 23:16:56 -0800

    Initial commit of weirdfishes

---
+++ weirdfishes/weirdfishes/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+url(r'^admin/', include(admin.site.urls)),
+
+url(r'^polls/', include('polls.urls', namespace="polls")),
+
+)
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
